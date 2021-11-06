5602415d88168b296b042b8c65268ed03cff4de9
voitovmichael <voitovmichael@yandex.ru>
2021-11-06 14:25:29 +0300

    feat: Добавить обработку заголовка

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -16,33 +16,34 @@ const createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        bcryptjs.hash(password, 10)
-          .then((hash) => {
-            User.create({
-              name, about, avatar, email, password: hash,
-            })
-              .then((newUser) => {
-                const { _id } = newUser;
-                res.status(200).send({
-                  _id, email, name, avatar,
-                });
-              })
-              .catch((err) => {
-                if (err.name === 'ValidationError') {
-                  next(new RequestError('Переданы некорректные данные для создании карточки.'));
-                } else {
-                  next(err);
-                }
-              });
-          })
-          .catch(next);
-      } else {
-        next(new SameDataError('Пользователь с таким email уже существует'));
-      }
-    });
+  res.status(400).send({ body: req.body });
+  // User.findOne({ email })
+  //   .then((user) => {
+  //     if (!user) {
+  //       bcryptjs.hash(password, 10)
+  //         .then((hash) => {
+  //           User.create({
+  //             name, about, avatar, email, password: hash,
+  //           })
+  //             .then((newUser) => {
+  //               const { _id } = newUser;
+  //               res.status(200).send({
+  //                 _id, email, name, avatar,
+  //               });
+  //             })
+  //             .catch((err) => {
+  //               if (err.name === 'ValidationError') {
+  //                 next(new RequestError('Переданы некорректные данные для создании карточки.'));
+  //               } else {
+  //                 next(err);
+  //               }
+  //             });
+  //         })
+  //         .catch(next);
+  //     } else {
+  //       next(new SameDataError('Пользователь с таким email уже существует'));
+  //     }
+  //   });
 };
 
 const getUser = (req, res, next) => {
